a6e9f65cb4c147d332a74618ced4f56dbd3eec46
joanavrsilva <joanavrsilva@gmail.com>
2022-03-18 02:42:27 +0000

    Give user a random answer

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -33,8 +34,20 @@ def welcome_message():
     question = input("Please type your question here:")
     questionsMade += 1
     print (f"So you ask this '{question}'.")
-    print ("Let me see what me eight ball says...")
-    print("ups...I mean...my crystal Ball.")
+    print ("Let me see what my magic eight ball says...")
+    print("ups...I mean...my crystal ball.")
+    print("The awnswer is...")
+
+    # Give user an answer
+    answers = ["It is certain.","It is decidedly so.",
+    "Without a doubt.","Yes definitely.","You may rely on it.",
+    "As I see it, yes.","Most likely.","Outlook good.","Yes.",
+    "Signs point to yes.","Reply hazy, try again.","Ask again later.",
+    "Better not tell you now.","Cannot predict now.",
+    "Concentrate and ask again.","Don't count on it.",
+    "My reply is no.","My sources say no.","Outlook not so good.",
+    "Very doubtful."]
+    print(random.choice(answers))
 
     # Ask if user wants to continue or leave
     print (f" {name} Do you want to ask me anything else?")
