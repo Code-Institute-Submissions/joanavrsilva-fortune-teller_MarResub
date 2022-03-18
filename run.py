28bb969bd727077bf9457bcb2f24c5fe6d6ef87e
joanavrsilva <joanavrsilva@gmail.com>
2022-03-18 07:59:57 +0000

    Add some spaces between texts

--- run.py
+++ run.py
@@ -18,16 +18,16 @@ def welcome_message():
     """
     Welcome message
     """
-    print("Welcome child to the Best Fortune Teller!")
+    print("Welcome child to the Best Fortune Teller!\n")
     print("You can ask me everything you want")
     print("...but if you want better results,")
-    print("I advice you to ask questions with Yes or No responses")
+    print("I advice you to ask questions with Yes or No responses\n")
     questionsMade = 0
 
     # Ask user name
     print("Let's start with your name")
 
-    name = input("Please type your name here:")
+    name = input("Please type your name: \n")
     print(f"So {name} what do you want to know?")
 
     # Ask user question
@@ -35,7 +35,7 @@ def welcome_message():
     questionsMade += 1
     print(f"So you ask this '{question}'.")
     print("Let me see what my magic eight ball says...")
-    print("ups...I mean...my crystal ball.")
+    print("ups...I mean...my crystal ball./n")
     print("The awnswer is...")
 
     # Give user an answer 
@@ -48,7 +48,7 @@ def welcome_message():
     # Ask if user wants to continue or leave
     print(f" {name} Do you want to ask me anything else?")
     
-    anotherQuestion = input("Please type Yes or No here: ")
+    anotherQuestion = input("Please type Yes or No: \n")
 
     if anotherQuestion.lower() != "yes":
         print(f"Bye {name} you ask me " + str(questionsMade) + " question(s)!")
