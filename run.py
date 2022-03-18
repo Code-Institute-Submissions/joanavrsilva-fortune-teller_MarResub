8ae002b00bc8e46780716ccad09e9be090ae70f0
joanavrsilva <joanavrsilva@gmail.com>
2022-03-18 09:19:54 +0000

    Fix pip8 errors: spaces, variables names, import position

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+"""
+bbb
+"""
+import random
 import gspread
 from google.oauth2.service_account import Credentials
-import random
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -22,7 +26,6 @@ def welcome_message():
     print("You can ask me everything you want")
     print("...but if you want better results,")
     print("I advice you to ask questions with Yes or No responses\n")
-    questionsMade = 0
 
     # Ask user name
     print("Let's start with your name")
@@ -31,32 +34,44 @@ def welcome_message():
     print(f"So {name} what do you want to know?")
 
     # Ask user question
-    question = input("Please type your question here:")
-    questionsMade += 1
+    question = input("Please type your question: \n")
     print(f"So you ask this '{question}'.")
     print("Let me see what my magic eight ball says...")
-    print("ups...I mean...my crystal ball./n")
+    print("ups...I mean...my crystal ball. \n")
     print("The awnswer is...")
 
-    # Give user an answer 
-    allMagicBallAnswers = SHEET.worksheet('allMagicBallAnswers')
+    # Give user an answer
+    all_magic_ball_answers = SHEET.worksheet('allMagicBallAnswers')
 
-    answerGiven = allMagicBallAnswers.col_values(1)
+    answer_given = all_magic_ball_answers.col_values(1)
 
-    print(random.choice(answerGiven))
+    print(random.choice(answer_given))
 
     # Ask if user wants to continue or leave
     print(f" {name} Do you want to ask me anything else?")
-    
-    anotherQuestion = input("Please type Yes or No: \n")
 
-    if anotherQuestion.lower() != "yes":
-        print(f"Bye {name} you ask me " + str(questionsMade) + " question(s)!")
+    another_question = input("Please type Yes or No: \n")
+
+    if another_question.lower() != "yes":
+        print(f"Bye {name} come back anytime!")
         quit()
-    
-    print("Okay! What do you want to know now?")
-    newQuestion = input("Please type your new question here: ")
-    questionsMade += 1
+
+    elif another_question.lower() == "yes":
+        print("Okay! What do you want to know now?")
+        new_question = input("Please type your new question here: ")
+        print(f"So you ask this '{new_question}'.")
+        print("Let me see what my magic eight ball says...")
+        print("ups...I mean...my crystal ball. \n")
+        print("The awnswer is...")
+
+        all_magic_ball_answers = SHEET.worksheet('allMagicBallAnswers')
+
+        answer_given = all_magic_ball_answers.col_values(1)
+
+        print(random.choice(answer_given))
+
+        print(f" {name} I hope I was able to answer some of your questions!")
+        quit()
 
 
 welcome_message()
